3c9d4aacd99fc81227d4bc644ccef103c7dd14d1
Administrator2 <re7an.ma7moud@gmail.com>
2023-07-24 22:15:33 +0300

    Removed old Java project files and added new project folder

---
+++ src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module testfolders {
+}
---
+++ src/test/java/com/example/software/AcceptanceTest/acceptanceTest.java
@@ -0,0 +1,13 @@
+package com.example.software.AcceptanceTest;
+
+        import io.cucumber.junit. Cucumber;
+        import io.cucumber.junit.CucumberOptions;
+        import org.junit.runner. RunWith;
+        import io.cucumber.junit. CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "cases", monochrome= true, snippets = SnippetType.CAMELCASE,  glue= {"com.example.software.AcceptanceTest"})
+
+public class acceptanceTest {
+
+}
